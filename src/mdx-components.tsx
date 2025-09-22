9f92a678aa74ebdbbc6094a526275be7492603ae
Eduardo Neville <eduardoneville82@gmail.com>
2025-09-22 16:17:09 +0200

    feat - initial docs (TLE, OEM, OCM)

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,10 +1,16 @@
-import defaultMdxComponents from 'fumadocs-ui/mdx';
+import defaultComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
+import { createGenerator } from 'fumadocs-typescript';
+import { AutoTypeTable } from 'fumadocs-typescript/ui';
 
-// use this function to get MDX components, you will need it for rendering MDX
+const generator = createGenerator();
+
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
-    ...defaultMdxComponents,
+    ...defaultComponents,
+    AutoTypeTable: (props) => (
+      <AutoTypeTable {...props} generator={generator} />
+    ),
     ...components,
   };
 }
